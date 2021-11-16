f3d591f065f5fce34d679646838fea085d04d93f
Felipe Cadavid <felipe.cadavid3@gmail.com>
2021-11-16 16:00:21 -0500

    WAButton

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+
+import './Homepage.css';
 
 import CategoryList from "../../components/CategoryList/CategoryList";
 import ComoFunciona from "../../components/ComoFunciona/ComoFunciona";
@@ -8,6 +12,16 @@ function Homepage() {
     <>
       <CategoryList />
       <ComoFunciona />
+      <div className="whatsapp-button-container">
+        <a
+          className="whatsapp-button"
+          href="https://api.whatsapp.com/send?phone=573022543198&text=Hola, me gustaría obtener más información sobre sus productos"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faWhatsapp} />
+        </a>
+      </div>
     </>
   );
 }
